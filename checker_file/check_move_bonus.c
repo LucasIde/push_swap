cefb8557a5f5da1fbec6609f4631891a90b70bb2
Lucas Ide <lide@fu-r10-p6.s19.be>
2022-03-24 15:46:54 +0100

    push rapide

--- checker_file/check_move_bonus.c
+++ checker_file/check_move_bonus.c
@@ -6,19 +6,14 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 19:49:53 by lide              #+#    #+#             */
-/*   Updated: 2022/03/24 14:30:19 by lide             ###   ########.fr       */
+/*   Updated: 2022/03/24 15:45:30 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
 
-int	check_move(char *str)
+int	check_move2(char *str, int i, int mv)
 {
-	int	i;
-	int	mv;
-
-	i = -1;
-	mv = 0;
 	while (str[++i] && mv >= 0)
 	{
 		if (str[i] == 's' && ((i == 0 && str[i + 1] != '\n')
@@ -29,9 +24,11 @@ int	check_move(char *str)
 			mv += 100;
 		else if (str[i] == 'p' && i == 0 && str[i + 1] != '\n')
 			mv += 1000;
-		else if (str[i] == 'a' && i > 0 && i < 3 && (mv % 10 == 0))
+		else if (str[i] == 'a' && (mv % 10 == 0) && (i == 1
+				|| (i == 2 && str[i - 1] == 'r')))
 			mv += 1;
-		else if (str[i] == 'b' && i > 0 && i < 3 && (mv % 10 == 0))
+		else if (str[i] == 'b' && (mv % 10 == 0) && (i == 1
+				|| (i == 2 && str[i - 1] == 'r')))
 			mv += 2;
 		else if (str[i] == '\n' && !str[i + 1])
 			mv += 3;
@@ -39,6 +36,16 @@ int	check_move(char *str)
 			mv = -1;
 	}
 	return (mv);
+}
+
+int	check_move(char *str)
+{
+	int	i;
+	int	mv;
+
+	i = -1;
+	mv = 0;
+	return (check_move2(str, i, mv));
 }
 
 int	move(t_list **list_a, t_list **list_b, char *str)
